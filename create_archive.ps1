477985224ecdfab2a8696120da7a16b689d653b1
avy1357 <163875356+avy1357@users.noreply.github.com>
2026-07-15 14:43:52 +0800

    Delete create_archive.ps1

--- create_archive.ps1
+++
@@ -1,8 +0,0 @@
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$archivePath = "d:\aliyun-sonicvale\zip归档\015_SonicVale_VolcanoVoiceIntegrationComplete.zip"
-$sourcePath = "d:\aliyun-sonicvale\SonicVale"
-if (Test-Path $archivePath) {
-    Remove-Item $archivePath -Force
-}
-Compress-Archive -Path $sourcePath -DestinationPath $archivePath -Force
-Write-Host "Done"
